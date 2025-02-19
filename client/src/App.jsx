47649f4d6ca7769647b4d9de680a96437a3dd25f
villayat <villayat.ali@proton.me>
2025-02-19 22:15:43 +0530

    Initial commit of japanime

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+
+    </>
+  )
+}
+
+export default App
